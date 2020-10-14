30fc73f20ea4affe8fff2d27fabcac24fe9a3f8c
Odett3 <67112041+Odett3@users.noreply.github.com>
2020-10-14 23:50:02 +0200

    filter by date

--- src/Components/OurWines.js
+++ src/Components/OurWines.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
 import wines from "../DB/wines.json"
 
 export default function OurWines() {
+
+  const [wineDate, setWineDate] = useState("")
+
+  let wineFilter;
+
+  if (wineDate === "2017") {
+
+    wineFilter = wines.filter((w)=> {return w.date === "2017"} 
+    
+    )
+  } else if (wineDate === "2018") {
+
+    wineFilter = wines.filter((w)=> {
+
+      return w.date === "2018"
+    })
+  } else {
+
+    wineFilter = wines
+  } 
+
   return (
     <div>
    <h1>Our Wines </h1>
 
-{wines.map((w) => { 
+<button onClick={()=> setWineDate("all")}> All </button>
+<button onClick={()=> setWineDate("2017")}> 2017 </button>
+<button onClick={()=> setWineDate("2018")}> 2018 </button>
 
-  return (<div>
-<h3>{w.name}</h3>
+{wineFilter.map((w) => { 
+
+  return (<div key={w.id}>
+<h3 > {w.name}</h3>
 <img src={w.image} alt="wine" width="40%"/>
 <p>{w.date}</p>
 <p>{w.description}</p>
