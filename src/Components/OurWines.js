9d4d63624dcc2840322dab56c72849a8fc3bf3c2
Odett3 <67112041+Odett3@users.noreply.github.com>
2020-10-14 23:17:19 +0200

    added json with current wines

--- src/Components/OurWines.js
+++ src/Components/OurWines.js
@@ -1,12 +1,23 @@
 import React from 'react';
-
+import wines from "../DB/wines.json"
 
 export default function OurWines() {
   return (
     <div>
-   
-      
-   OurWines
+   <h1>Our Wines </h1>
+
+{wines.map((w) => { 
+
+  return (<div>
+<h3>{w.name}</h3>
+<img src={w.image} alt="wine" width="40%"/>
+<p>{w.date}</p>
+<p>{w.description}</p>
+  
+</div>)
+
+})}
+
 
     </div>
   );
